7992bf1c5c56e8c397b41cec5d6fdf3424803696
Shreyash Sutar <shreyashsutar0404@gmail.com>
2025-02-01 14:39:03 +0530

    Feat:
    	1. Handles student-related endpoints using `@RestController`.
    	2. Returns a `Student` object as JSON via `getStudent()`.
    	3. Used produces attribute to decide the return data type i.e
    	   ==> xml or json

---
+++ src/main/java/org/dnyanyog/controller/StudentController.java
@@ -0,0 +1,26 @@
+package org.dnyanyog.controller;
+
+import org.dnyanyog.dto.Students;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Student")
+public class StudentController {
+	
+	@GetMapping(path="/studentInfo", produces={"application/json"}) //here after produces if you write {"application.json"} it will return your object in json format 
+	public Students std() {											// if you write application/xml it will return your object in xml format.
+		Students std = new Students();
+		std.firstName = "Shreyash";
+		std.lastName = "Sutar";
+		std.email = "shreyashsutar124@gmail.com";
+		std.mobileNumber = "8485898732";
+		std.age = 21;
+		
+		return std;
+	}
+
+
+}
